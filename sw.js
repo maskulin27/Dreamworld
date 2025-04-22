a542419175b420c2b0f533d4949cbc245a2d6e1d
maskulin27 <maskulin2@gmail.com>
2025-04-22 11:47:42 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'fc-europa-v1';
+const FILES_TO_CACHE = [
+  '/index.html',
+  '/fc-europa-pl.html',
+  '/fc-europa-cz.html',
+  '/fc-europa-de.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => Promise.all(
+      cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+    ))
+  );
+});
